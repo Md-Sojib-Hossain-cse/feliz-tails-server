68846394492f6ac936f79d588e5c18b8fe359bfe
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-09-04 22:52:26 +0600

    database commented and create reviews get api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -8,10 +11,44 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get("/", async (req, res) => {
-    res.send("Feliz Tails server is running .....");
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnc3ejx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.listen(port, () => {
-    console.log(`Feliz Tails server is running on port : ${port}`);
-})
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const reviewsCollection = client.db("felizTailsDB").collection("reviews");
+
+        app.get("/" ,async(req, res) => {
+            res.send("Feliz Tails Server is running ...");
+        })
+
+
+        //reviews related api
+        app.get("/reviews" , async(req , res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
