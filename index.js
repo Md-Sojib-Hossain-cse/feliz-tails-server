1ede295847631460c72566f432fcef1634f87051
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-09-07 02:25:44 +0600

    jwt api added with verify token middleware and verify admin middleware using json-webtoken and cookie parser

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173', 'http://localhost:5174',
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+//custom middleware
+const verifyToken = (req , res , next) => {
+    const token = req?.cookies?.token;
+    console.log(token , req.cookie , req.cookie?.token)
+    if(!token){
+        return res.status(401).send({message : "unauthorized access"})
+    }
+    jwt.verify(token , process.env.ACCESS_TOKEN_SECRET ,  (err , decoded) => {
+        if(err){
+            return res.status(401).send({message : "unauthorized access"})
+        }
+        req.user = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnc3ejx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,6 +45,12 @@ const client = new MongoClient(uri, {
     }
 });
 
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -30,19 +60,44 @@ async function run() {
         const petListingCollection = client.db("felizTailsDB").collection("petListing");
         const usersCollection = client.db("felizTailsDB").collection("users");
 
+
+        //custom middleware 
+        const verifyAdmin = async(req , res , next) => {
+            const user =req?.user;
+            console.log(user?.email)
+            const query = {email : user?.email};
+            const result = await usersCollection.findOne(query)
+            console.log(result)
+            if(result.role !== "admin"){
+                return res.status(401).send({message : "unauthorized access"})
+            }
+            next();
+        }
+
         app.get("/", async (req, res) => {
             res.send("Feliz Tails Server is running ...");
+        })
+
+        //jwt related api
+        app.post("/jwt", async (req, res) => {
+            const token = await jwt.sign(req.body, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "24hr" })
+            res.cookie("token" , token , cookieOptions).send({success : true})
         })
 
         //user related api
         app.post("/users", async (req, res) => {
             const userInfo = req.body;
+            const query = { email: userInfo.email };
+            const isExist = await usersCollection.findOne(query);
+            if (isExist) {
+                return;
+            }
             const result = await usersCollection.insertOne(userInfo);
             res.send(result);
         })
 
         //pet listing related api
-        app.get("/pet-listing", async (req, res) => {
+        app.get("/pet-listing",  async (req, res) => {
             const searchedCategory = req.query.category;
             const searchedName = req.query.name;
             const page = req.query.page;
